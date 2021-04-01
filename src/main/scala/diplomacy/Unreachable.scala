b4dd79a8af249298f8defa49c874022541018ea7
Ruige Lee <295054118@qq.com>
2021-04-01 09:22:59 +0800

    remove rocket

--- src/main/scala/diplomacy/Unreachable.scala
+++
@@ -1,7 +0,0 @@
-// See LICENSE.SiFive for license details.
-
-package freechips.rocketchip.diplomacy
-
-case object Unreachable {
-  def apply(): Nothing = throw new AssertionError("unreachable code")
-}
