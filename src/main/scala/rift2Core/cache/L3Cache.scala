930c683364ddedee2fbe00e151b5afa362507438
Ruige Lee <295054118@qq.com>
2021-05-11 14:47:55 +0800

    only 32 cycle is needed in axi, because the data width is 128bit, 2 times lager than 64bit

--- src/main/scala/rift2Core/cache/L3Cache.scala
+++ src/main/scala/rift2Core/cache/L3Cache.scala
@@ -503,7 +503,7 @@ class L3Cache ( dw:Int = 1024, bk:Int = 4, cl:Int = 256 ) extends Module {
 
 	mem_mst_w.io.aw_info.burst := "b01".U
 	mem_mst_w.io.aw_info.id := 0.U
-	mem_mst_w.io.aw_info.len := 63.U
+	mem_mst_w.io.aw_info.len := 31.U
 	mem_mst_w.io.aw_info.size := 4.U
 	mem_mst_w.io.aw_info.user := 0.U
 	mem_mst_w.io.aw_info.cache := 0.U
