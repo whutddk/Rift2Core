0a880160cbef067eea9818315597f9994879b11c
Ruige Lee <295054118@qq.com>
2021-07-27 10:41:26 +0800

    re-orginaze pending fifo

--- src/main/scala/rift2Core/backend/Lsu_pending_fifo.scala
+++ src/main/scala/rift2Core/backend/Lsu_pending_fifo.scala
@@ -83,70 +83,221 @@ class lsu_pending_fifo(val entries: Int)(implicit p: Parameters) extends DcacheM
   val is_hazard = buf.exists( (x:Info_cache_sb) => (x.param.op1(31, addr_lsb) === io.enq.bits.param.op1(31, addr_lsb)) )
   io.is_hazard := is_hazard
 
-  when (do_enq & ~io.flush) {
-    buf(enq_ptr(cnt_w-1,0)) := io.enq.bits
-    valid(enq_ptr(cnt_w-1,0)) := true.B
+  // when (do_enq & ~io.flush) {
+  //   buf(enq_ptr(cnt_w-1,0)) := io.enq.bits
+  //   valid(enq_ptr(cnt_w-1,0)) := true.B
 
-    enq_ptr := enq_ptr + 1.U
-  }
-  when (do_deq) {
-    buf(deq_ptr(cnt_w-1,0)) := 0.U.asTypeOf(new Info_cache_sb)
-    valid(deq_ptr(cnt_w-1,0)) := false.B
-    commit(deq_ptr(cnt_w-1,0)) := false.B
+  //   enq_ptr := enq_ptr + 1.U
+  // }
+  // when (do_deq) {
+  //   buf(deq_ptr(cnt_w-1,0)) := 0.U.asTypeOf(new Info_cache_sb)
+  //   valid(deq_ptr(cnt_w-1,0)) := false.B
+  //   commit(deq_ptr(cnt_w-1,0)) := false.B
 
-    deq_ptr := deq_ptr + 1.U
-  }
-  when( do_cmm ) {
-    commit(cmm_ptr(cnt_w-1,0)) := true.B
-    commit(cmm_ptr(cnt_w-1,0) + 1.U) := io.cmm.bits
+  //   deq_ptr := deq_ptr + 1.U
+  // }
+  // when( do_cmm ) {
+  //   commit(cmm_ptr(cnt_w-1,0)) := true.B
+  //   commit(cmm_ptr(cnt_w-1,0) + 1.U) := io.cmm.bits
 
-    cmm_ptr := cmm_ptr + Mux(io.cmm.bits, 2.U, 1.U)
+  //   cmm_ptr := cmm_ptr + Mux(io.cmm.bits, 2.U, 1.U)
 
-    when( io.flush ) {
-      enq_ptr := cmm_ptr + Mux(io.cmm.bits, 2.U, 1.U)
+  //   when( io.flush ) {
+  //     enq_ptr := cmm_ptr + Mux(io.cmm.bits, 2.U, 1.U)
 
-      for ( i <- 0 until entries ) yield {
-        buf(i) := 
-          Mux( commit(i) | (i.U === cmm_ptr(cnt_w-1,0)) | (i.U === (cmm_ptr(cnt_w-1,0) + 1.U) & io.cmm.bits), buf(i), 0.U.asTypeOf(new Info_cache_sb))
+  //     for ( i <- 0 until entries ) yield {
+  //       buf(i) := 
+  //         Mux( commit(i) | (i.U === cmm_ptr(cnt_w-1,0)) | (i.U === (cmm_ptr(cnt_w-1,0) + 1.U) & io.cmm.bits), buf(i), 0.U.asTypeOf(new Info_cache_sb))
 
-        valid(i) := 
-          Mux( commit(i) | (i.U === cmm_ptr(cnt_w-1,0)) | (i.U === (cmm_ptr(cnt_w-1,0) + 1.U) & io.cmm.bits), valid(i), false.B)
-      }
-    }
+  //       valid(i) := 
+  //         Mux( commit(i) | (i.U === cmm_ptr(cnt_w-1,0)) | (i.U === (cmm_ptr(cnt_w-1,0) + 1.U) & io.cmm.bits), valid(i), false.B)
+  //     }
+  //   }
+  //   assert( cmm_ptr =/= enq_ptr )
+  //   assert( ~io.amo & ~do_amo )
+  // } 
+  // .elsewhen( buf(cmm_ptr(cnt_w-1,0)).fun.is_amo & valid(cmm_ptr(cnt_w-1,0)) === true.B & do_amo) {
+  //   commit(cmm_ptr(cnt_w-1,0)) := true.B
+  //   cmm_ptr := cmm_ptr + 1.U
 
+  //   when( io.flush ) {
+  //     enq_ptr := cmm_ptr + 1.U
 
-    assert( cmm_ptr =/= enq_ptr )
-    assert( ~io.amo & ~do_amo )
-  } 
-  .elsewhen( buf(cmm_ptr(cnt_w-1,0)).fun.is_amo & valid(cmm_ptr(cnt_w-1,0)) === true.B & do_amo) {
-    commit(cmm_ptr(cnt_w-1,0)) := true.B
-    cmm_ptr := cmm_ptr + 1.U
+  //     for ( i <- 0 until entries ) yield {
+  //       buf(i) :=
+  //         Mux( commit(i) | (i.U === cmm_ptr(cnt_w-1,0)), buf(i), 0.U.asTypeOf(new Info_cache_sb))
+  //       valid(i) :=
+  //         Mux( commit(i) | (i.U === cmm_ptr(cnt_w-1,0)), valid(i), false.B)
+  //     }      
+  //   }
+  //   assert( cmm_ptr =/= enq_ptr )
+  //   assert( ~do_cmm )
+  // }
+  // .otherwise {
+  //   when( io.flush ) {
+  //     enq_ptr := cmm_ptr
 
-    when( io.flush ) {
-      enq_ptr := cmm_ptr + 1.U
-
-      for ( i <- 0 until entries ) yield {
-        buf(i) :=
-          Mux( commit(i) | (i.U === cmm_ptr(cnt_w-1,0)), buf(i), 0.U.asTypeOf(new Info_cache_sb))
-        valid(i) :=
-          Mux( commit(i) | (i.U === cmm_ptr(cnt_w-1,0)), valid(i), false.B)
-      }      
-    }
-
-
-    assert( cmm_ptr =/= enq_ptr )
-    assert( ~do_cmm )
-  }
-
-
-  // when( io.flush ) {
-  //   enq_ptr := cmm_ptr
-
-  //   for ( i <- 0 until entries ) yield {
-  //     buf(i) := Mux( commit(i), buf(i), 0.U.asTypeOf(new Info_cache_sb))
-  //     valid(i) := Mux( commit(i), valid(i), false.B)
+  //     for ( i <- 0 until entries ) yield {
+  //       buf(i) := Mux( commit(i), buf(i), 0.U.asTypeOf(new Info_cache_sb))
+  //       valid(i) := Mux( commit(i), valid(i), false.B)
+  //     }
   //   }
   // }
+
+
+    when( ~io.flush ) {
+      when (do_enq) {
+        buf(enq_ptr(cnt_w-1,0)) := io.enq.bits
+        valid(enq_ptr(cnt_w-1,0)) := true.B
+
+        enq_ptr := enq_ptr + 1.U
+      }
+      when (do_deq) {
+        buf(deq_ptr(cnt_w-1,0)) := 0.U.asTypeOf(new Info_cache_sb)
+        valid(deq_ptr(cnt_w-1,0)) := false.B
+        commit(deq_ptr(cnt_w-1,0)) := false.B
+
+        deq_ptr := deq_ptr + 1.U
+      }
+
+      when( do_cmm ) {
+        commit(cmm_ptr(cnt_w-1,0)) := true.B
+        commit(cmm_ptr(cnt_w-1,0) + 1.U) := io.cmm.bits
+
+        cmm_ptr := cmm_ptr + Mux(io.cmm.bits, 2.U, 1.U)
+
+        assert( cmm_ptr =/= enq_ptr )
+        assert( ~io.amo & ~do_amo )
+      }
+      .elsewhen( buf(cmm_ptr(cnt_w-1,0)).fun.is_amo & valid(cmm_ptr(cnt_w-1,0)) === true.B & do_amo) {
+        commit(cmm_ptr(cnt_w-1,0)) := true.B
+        cmm_ptr := cmm_ptr + 1.U
+
+        assert( cmm_ptr =/= enq_ptr )
+        assert( ~do_cmm )
+      }
+    }
+    .otherwise {
+
+      enq_ptr := cmm_ptr
+
+      for ( i <- 0 until entries ) yield {
+        buf(i) := Mux( commit(i), buf(i), 0.U.asTypeOf(new Info_cache_sb))
+        valid(i) := Mux( commit(i), valid(i), false.B)
+      }
+
+      when (do_deq) {
+        buf(deq_ptr(cnt_w-1,0)) := 0.U.asTypeOf(new Info_cache_sb)
+        valid(deq_ptr(cnt_w-1,0)) := false.B
+        commit(deq_ptr(cnt_w-1,0)) := false.B
+
+        deq_ptr := deq_ptr + 1.U
+      }
+
+      when( do_cmm ) {
+        commit(cmm_ptr(cnt_w-1,0)) := true.B
+        commit(cmm_ptr(cnt_w-1,0) + 1.U) := io.cmm.bits
+
+        cmm_ptr := cmm_ptr + Mux(io.cmm.bits, 2.U, 1.U)
+
+        enq_ptr := cmm_ptr + Mux(io.cmm.bits, 2.U, 1.U)
+
+        buf(cmm_ptr(cnt_w-1,0)) := buf(cmm_ptr(cnt_w-1,0))
+        buf(cmm_ptr(cnt_w-1,0) + 1.U) := Mux(io.cmm.bits, buf(cmm_ptr(cnt_w-1,0) + 1.U), 0.U.asTypeOf(new Info_cache_sb))
+        
+        valid(cmm_ptr(cnt_w-1,0)) := valid(cmm_ptr(cnt_w-1,0))
+        valid(cmm_ptr(cnt_w-1,0) + 1.U) := Mux(io.cmm.bits, valid(cmm_ptr(cnt_w-1,0) + 1.U), false.B)
+
+        assert( cmm_ptr =/= enq_ptr )
+        assert( ~io.amo & ~do_amo )
+      }
+      .elsewhen( buf(cmm_ptr(cnt_w-1,0)).fun.is_amo & valid(cmm_ptr(cnt_w-1,0)) === true.B & do_amo) {
+        commit(cmm_ptr(cnt_w-1,0)) := true.B
+        cmm_ptr := cmm_ptr + 1.U
+
+
+        enq_ptr := cmm_ptr + 1.U
+
+        buf(cmm_ptr(cnt_w-1,0)) := buf(cmm_ptr(cnt_w-1,0))
+        valid(cmm_ptr(cnt_w-1,0)) := valid(cmm_ptr(cnt_w-1,0))
+
+
+        assert( cmm_ptr =/= enq_ptr )
+        assert( ~do_cmm )
+      }     
+      
+    }
+
+
+  // when (do_enq & ~io.flush) {
+  //   buf(enq_ptr(cnt_w-1,0)) := io.enq.bits
+  //   valid(enq_ptr(cnt_w-1,0)) := true.B
+
+  //   enq_ptr := enq_ptr + 1.U
+  // }
+  // when (do_deq) {
+  //   buf(deq_ptr(cnt_w-1,0)) := 0.U.asTypeOf(new Info_cache_sb)
+  //   valid(deq_ptr(cnt_w-1,0)) := false.B
+  //   commit(deq_ptr(cnt_w-1,0)) := false.B
+
+  //   deq_ptr := deq_ptr + 1.U
+  // }
+  // when( do_cmm ) {
+  //   commit(cmm_ptr(cnt_w-1,0)) := true.B
+  //   commit(cmm_ptr(cnt_w-1,0) + 1.U) := io.cmm.bits
+
+  //   cmm_ptr := cmm_ptr + Mux(io.cmm.bits, 2.U, 1.U)
+
+  //   when( io.flush ) {
+  //     enq_ptr := cmm_ptr + Mux(io.cmm.bits, 2.U, 1.U)
+
+  //     for ( i <- 0 until entries ) yield {
+  //       buf(i) := 
+  //         Mux( commit(i) | (i.U === cmm_ptr(cnt_w-1,0)) | (i.U === (cmm_ptr(cnt_w-1,0) + 1.U) & io.cmm.bits), buf(i), 0.U.asTypeOf(new Info_cache_sb))
+
+  //       valid(i) := 
+  //         Mux( commit(i) | (i.U === cmm_ptr(cnt_w-1,0)) | (i.U === (cmm_ptr(cnt_w-1,0) + 1.U) & io.cmm.bits), valid(i), false.B)
+  //     }
+  //   }
+  //   assert( cmm_ptr =/= enq_ptr )
+  //   assert( ~io.amo & ~do_amo )
+  // } 
+  // .elsewhen( buf(cmm_ptr(cnt_w-1,0)).fun.is_amo & valid(cmm_ptr(cnt_w-1,0)) === true.B & do_amo) {
+  //   commit(cmm_ptr(cnt_w-1,0)) := true.B
+  //   cmm_ptr := cmm_ptr + 1.U
+
+  //   when( io.flush ) {
+  //     enq_ptr := cmm_ptr + 1.U
+
+  //     for ( i <- 0 until entries ) yield {
+  //       buf(i) :=
+  //         Mux( commit(i) | (i.U === cmm_ptr(cnt_w-1,0)), buf(i), 0.U.asTypeOf(new Info_cache_sb))
+  //       valid(i) :=
+  //         Mux( commit(i) | (i.U === cmm_ptr(cnt_w-1,0)), valid(i), false.B)
+  //     }      
+  //   }
+  //   assert( cmm_ptr =/= enq_ptr )
+  //   assert( ~do_cmm )
+  // }
+  // .otherwise {
+  //   when( io.flush ) {
+  //     enq_ptr := cmm_ptr
+
+  //     for ( i <- 0 until entries ) yield {
+  //       buf(i) := Mux( commit(i), buf(i), 0.U.asTypeOf(new Info_cache_sb))
+  //       valid(i) := Mux( commit(i), valid(i), false.B)
+  //     }
+  //   }
+  // }
+
+
+
+
+
+
+
+
+
 
   io.deq.valid := deq_ptr =/= cmm_ptr
   io.enq.ready := !full
--- tb/rift2chip_tb.v
+++ tb/rift2chip_tb.v
@@ -4,7 +4,7 @@
 * @Email: wut.ruigeli@gmail.com
 * @Date:   2021-04-21 15:17:49
 * @Last Modified by:   Ruige Lee
-* @Last Modified time: 2021-07-22 19:53:36
+* @Last Modified time: 2021-07-27 09:58:40
 */
 
 
@@ -439,7 +439,7 @@ reg [7:0] mem [0:200000];
 localparam DP = 2**14;
 integer i, by;
 initial begin
-	// $readmemh("./ci/rv64ui-p-fence_i.verilog", mem);
+	// $readmemh("./ci/rv64ui-p-sb.verilog", mem);
 	$readmemh(testName, mem);
 	
 	for ( i = 0; i < DP; i = i + 1 ) begin
