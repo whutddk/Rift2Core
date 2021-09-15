5bf9c15140ddead825c24ff3ecb7e1a1106b3713
Ruige Lee <295054118@qq.com>
2021-09-15 20:08:41 +0800

    i can not get the info of dromajo

--- tb/verilator/dromajo.cpp
+++ tb/verilator/dromajo.cpp
@@ -1,17 +1,18 @@
 /*
 * @Author: Ruige Lee
-* @Date:   2021-08-06 10:14:14
+* @Date:   2021-09-15 15:37:08
 * @Last Modified by:   Ruige Lee
-* @Last Modified time: 2021-09-13 20:05:10
+* @Last Modified time: 2021-09-15 20:01:08
 */
 
+#include "dromajo_cosim.h"
 
 #include <verilated.h>
-#include "VSimTop.h"
+#include "Vtest.h"
 #include <memory>
 #include <iostream>
 #include <getopt.h>
-#include "sim.h"
+
 
 #if VM_TRACE
 #include "verilated_vcd_c.h"
@@ -64,20 +65,9 @@ int main(int argc, char **argv, char **env) {
 		return -1;
 	}
 
-
-	// sim_t s(isa, priv, varch, nprocs, halted, real_time_clint,
-	//       initrd_start, initrd_end, bootargs, start_pc, mems, plugin_devices, htif_args,
-	//       std::move(hartids), dm_config, log_path, dtb_enabled, dtb_file
-	//       , io_service_ptr, acceptor_ptr
-	//       );
-
-
-
-
-
 	Verilated::commandArgs(argc, argv);
 
-	VSimTop *top = new VSimTop();
+	Vtest *top = new Vtest();
 
 #if VM_TRACE
 	VerilatedVcdC* tfp = new VerilatedVcdC;;
@@ -108,17 +98,12 @@ int main(int argc, char **argv, char **env) {
 			top->CLK = 0;
 		} 
 
-
-
-
-
 		top->eval();
 
 #if VM_TRACE
 		if ( flag_waveEnable ) {
 			tfp->dump(Verilated::time());			
 		}
-
 #endif
 
 		if ( main_time > 500000 ){
@@ -126,14 +111,14 @@ int main(int argc, char **argv, char **env) {
 			break;
 		} 
 
-		if ( top -> fail == 1 && main_time % 100 == 0 ) {
-			std::cout << "Fail!!!" << std::endl;	
-			break;			
-		}
-		else if ( top -> success == 1 && main_time % 100 == 0 ) {
-			std::cout << "Pass!" << std::endl;	
-			break;			
-		} 
+		// if ( top -> fail == 1 && main_time % 100 == 0 ) {
+		// 	std::cout << "Fail!!!" << std::endl;	
+		// 	break;			
+		// }
+		// else if ( top -> success == 1 && main_time % 100 == 0 ) {
+		// 	std::cout << "Pass!" << std::endl;	
+		// 	break;			
+		// } 
 
 
 		main_time ++;
@@ -146,21 +131,35 @@ int main(int argc, char **argv, char **env) {
 #endif
 	top->final();
 
+
+std::cout << "dromajo_cosim_init" << std::endl;	
+
+    dromajo_cosim_state_t *costate = 0;
+    costate                        = dromajo_cosim_init(argc, argv);
+
+    if (!costate)
+        return 1;
+
+std::cout << "dromajo_cosim_step" << std::endl;	
+
+    while (!dromajo_cosim_step(costate, 0, 0, 0, 0, 0, false)) {
+    	RISCVMachine m;
+    	m = *(RISCVMachine *)costate;
+    	std::cout << m->RISCVCPUState[0]->pc << std::endl;	
+    	std::cout << m->RISCVCPUState[0]->reg[0] << std::endl;	
+    	std::cout << m->RISCVCPUState[0]->reg[1] << std::endl;	
+    }
+
+
+std::cout << "dromajo_cosim_fini" << std::endl;	
+    dromajo_cosim_fini(costate);
+
+
+
 	delete top;
 
-
-	
 	return 0;
 
-
-
-
 }
-
-
-
-
-
-
 
 
