f6740c9d6515cd6ae839909a8bb30187270512d5
Ruige Lee <295054118@qq.com>
2022-02-02 16:20:29 +0800

    bugfix: check zipqueue full

--- src/main/scala/base/ZipQueue.scala
+++ src/main/scala/base/ZipQueue.scala
@@ -36,11 +36,11 @@ class ZipQueue[T<:Data]( dw: T, aw: Int, in: Int, out: Int ) extends Module{
   val is_zipped  = Wire(Vec(dp,Vec(3,  Bool())))
   val zipping = Wire(Vec(dp, UInt(2.W)))
 
-
+  val is_full = valid(dp-1) === true.B
   wr_ptr := 
     Mux(
-      valid.forall((x:Bool) => (x === false.B)), 0.U,
-      valid.lastIndexWhere((x:Bool) => (x === true.B)) + 1.U
+      is_full, 0.U,
+      valid.lastIndexWhere((x:Bool) => (x === false.B))
   )
 
 
@@ -108,52 +108,8 @@ class ZipQueue[T<:Data]( dw: T, aw: Int, in: Int, out: Int ) extends Module{
   }
 
 
-
-
-
-
-
-
-
-
-
-  // for ( i <- 0 until dp ) {
-  //   when( valid(i) === true.B ) {
-  //     when( is_zip(i).exists((x:Bool) => (x === true.B))){
-  //       buff(i) := 0.U.asTypeOf(dw)
-  //       valid(i) := false.B
-  //     } .otherwise {  } //is_zip(i) === false.B
-  //   } .otherwise { //valid(i) === false.B
-  //     if ( i < dp - 4 ) {
-  //       when( valid(i+1) === true.B && is_zip(i+1)(1) === false.B && is_zip(i+1)(2) === false.B) {
-  //         buff(i) := buff(i+1)
-  //         valid(i) := valid(i+1)
-  //         is_zip(i+1)(0) := true.B
-  //       } .elsewhen( valid(i+2) === true.B && is_zip(i+2)(2) === false.B) {
-  //         buff(i) := buff(i+2)
-  //         valid(i) := valid(i+2)
-  //         is_zip(i+2)(1) := true.B
-  //       } .elsewhen( valid(i+3) === true.B ) {
-  //         buff(i) := buff(i+3)
-  //         valid(i) := valid(i+3)
-  //         is_zip(i+3)(2) := true.B
-  //       }        
-  //     }
-
-  //     for ( j <- 0 until in ) yield {  
-  //       when( io.enq(j).fire && (i.U === wr_ptr+j.U) ) {
-  //         buff(i) := io.enq(j).bits
-  //         valid(i) := true.B
-
-  //         assert( is_zip(i).forall((x:Bool) => (x === false.B)) )
-  //       }
-  //     }
-  //   }
-
-  // }
-
   for ( i <- 0 until in ) yield {  
-    io.enq(i).ready := (valid(wr_ptr+i.U) === false.B) & (wr_ptr < (dp-i).U)
+    io.enq(i).ready := (valid(wr_ptr+i.U) === false.B) & (wr_ptr < (dp-i).U) & ~is_full
   }
 
   for ( i <- 0 until out ) yield {
@@ -165,7 +121,5 @@ class ZipQueue[T<:Data]( dw: T, aw: Int, in: Int, out: Int ) extends Module{
   for ( i <- 0 until in; j <- 0 until in ) yield 
     assert( !(io.enq(i).valid === true.B && io.enq(j).valid === false.B && i.U >= j.U), "Assert Fail! enq port illegal")
 
-  // for ( i <- 0 until out; j <- 0 until out ) yield 
-  //   assert( !(io.deq(i).valid === true.B && io.deq(j).valid === false.B && i.U >= j.U), "Assert Fail! deq port illegal")
 
 }
