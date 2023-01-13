7f4a2578d1c2ce642c190c47dba351c2c098c498
Ruige Lee <295054118@whut.edu.cn>
2023-01-13 12:03:32 +0000

    Pending changes exported from your codespace

--- src/main/scala/rift2Core/vector/VLSU.scala
+++ src/main/scala/rift2Core/vector/VLSU.scala
@@ -98,35 +98,133 @@ abstract class VLSUBase (val edge: Seq[TLEdgeOut])(implicit p: Parameters) exten
 
 
 
-    val lsu_iss_exe = Flipped(new DecoupledIO(new Lsu_iss_info))
-    val lsu_exe_iwb = new DecoupledIO(new WriteBack_info(dw=64))
-    val lsu_exe_fwb = new DecoupledIO(new WriteBack_info(dw=65))
 
+
+
+}
+
+
+class VLSU_Entry_Bundle extends RiftBundle{
+
+  val info        = Vec( vlen/8, new Lsu_iss_info)
+  val isRequested = Vec( vlen/8, Bool() )
+  val isWroteBack = Vec( vlen/8, Bool() )
+  val isException = Vec( vlen/8, Bool() )
+  val res         = Vec( vlen/8, UInt(8.W) )          
+
+  def isMasked    = Vec( vlen/8, Bool() )
+  def vstart      = UInt((log2Ceil(vlen/8)).W)
+  def rd0         = UInt((log2Ceil(vRegNum)).W)
+  def nextOp      = 
 
 }
 
 trait VLSUScoreboard{ this: => VLSUBase
-  val VReqBuf = Vec ( Vec (), Lsu_iss_info)
-  val VRspBuf = Vec ( Vec (), Lsu_iss_info)
+  def entry = 16
+
+  val sbBuf = Reg( Vec( entry, new VLSU_Entry_Bundle) )
+
+
+  val sbISSPtr = RegInit( 0.U((log2Ceil(entry)+1).W) )
+  val sbEXEPtr = RegInit( 0.U((log2Ceil(entry)+1).W) )
+  val sbVWBPtr = RegInit( 0.U((log2Ceil(entry)+1).W) )
+
+  val isSBEmpty = sbISSPtr === sbEXEPtr
+  val isSBFull  = (sbIWBPtr.extract(log2Ceil(entry)) =/= sbISSPtr.extract(log2Ceil(entry))) & (sbIWBPtr(log2Ceil(entry)-1,0) === sbISSPtr.extract(log2Ceil(entry)-1,0))
+
+}
+
+trait VLSUEnq{ this: => VLSUBase
+  when( isSBEmpty & ~io.lsu_iss_exe.bits.isVpu ){ //bypass normal lsu request
+    lsu.io.lsu_iss_exe <> io.lsu_iss_exe
+  } .otherwise{
+    io.lsu_iss_exe.ready := ~isSBFull
+
+    when( io.lsu_iss_exe.fire ){
+      sbBuf(sbISSPtr).info := Pkg_VLSUSB_Info(io.lsu_iss_exe.bits)
+      sbBuf(sbISSPtr).isRequested := 0.U.asTypeof(Vec( vlen/8, Bool() ))
+      sbBuf(sbISSPtr).isWroteBack := 0.U.asTypeof(Vec( vlen/8, Bool() ))
+      sbBuf(sbISSPtr).isException := 0.U.asTypeof(Vec( vlen/8, Bool() ))
+      sbISSPtr := sbISSPtr + 1.U
+    }
+
+    when( ~isSBEmpty ){
+      def exeIdx 
+      lsu.io.lsu_iss_exe.valid := true.B
+      lsu.io.lsu_iss_exe.bits := sbBuf(sbEXEPtr).info(exeIdx)
+
+      when( lsu.io.lsu_iss_exe.fire ){
+        assert( sbBuf(sbEXEPtr).isRequested(exeIdx) === false.B )
+        sbBuf(sbEXEPtr).isRequested(exeIdx) := true.B
+        when( PopCount( sbBuf(sbEXEPtr).isRequested.map{~_} ) === 1.U ){
+
+          sbEXEPtr := sbEXEPtr + 1.U
+        }
+      }
+    } .otherwise{
+      lsu.io.lsu_iss_exe.valid := false.B
+      lsu.io.lsu_iss_exe.bits  := 0.U.asTypeof(new Lsu_iss_info)
+    }
+
+  }
+}
+
+trait VLSUDeq{ this: => VLSUBase
+  io.lsu_exe_fwb <> lsu.io.lsu_exe_fwb
+
+  val bufIdx = lsu.io.lsu_exe_iwb.bits.bufIdx
+  val eleIdx = lsu.io.lsu_exe_iwb.bits.eleIdx
+
+  when( bufIdx === entry ){
+    io.lsu_exe_iwb <> lsu.io.lsu_exe_iwb
+  } .otherwise{
+    lsu.io.lsu_exe_iwb.ready := true.B
+
+    when( lsu.io.lsu_exe_iwb.fire ){
+      sbBuf(bufIdx).res(eleIdx)         := Pkg_VLSUSB_RES()
+      sbBuf(bufIdx).isWroteBack(eleIdx) := true.B
+      sbBuf(bufIdx).isException(eleIdx) :=   
+
+      when( PopCount(sbBuf(bufIdx).isWroteBack.map{~_}) === 1.U ){
+        io.lsu_exe_vwb.valid := true.B
+        io.lsu_exe_vwb.bits  := Pkg_VLSUWB_info()
+      }
+    }
+
+  }
+
+  when(io.lsu_exe_vwb.fire){
+    sbVWBPtr := sbVWBPtr + 1.U
+  }
+
+
+}
+
+class Vector_Request_Bundle extends RiftBundle{
+  val res         = Vec( vlen/8, UInt(8.W) )
+  val op          = Vec( vlen/8, new Lsu_iss_info)
+  val isRequested = Vec( vlen/8, Bool() )
+  val isMasked    = Vec( vlen/8, Bool() )
+  val vstart      = UInt((log2Ceil(vlen/8)).W)
+  val rd0         = UInt((log2Ceil(vRegNum)).W)
 }
 
 
 
-  val        = Bool()
-  val       = Bool()
-  val vluxei    = Bool()
-  val vloxei    = Bool()
-  val     = Bool()
-
-  val vlNreN    = Bool()
-
-  val        = Bool()
-
-  val       = Bool()
-  val vsuxei    = Bool()
-  val vsoxei    = Bool()
-  val vsNr      = Bool()
+class Vector_WriteBack_Bundle extends RiftBundle{
+  val res         = Vec( vlen/8, UInt(8.W) )
+  val isWroteBack = Vec( vlen/8, Bool() )
+  val isMasked    = Vec( vlen/8, Bool() )
+  val isInvalid   = Vec( vlen/8, Bool() )
+  val vstart      = UInt((log2Ceil(8*vlen/8)).W)
+  val rd0         = UInt((log2Ceil(vRegNum)).W)
 }
+
+
+
+
+
+
 
 trait Rsp{
   when (vle vse vlse vsse ){
@@ -138,4 +236,8 @@ trait Rsp{
   when(vleff){
     
   }
+  when( vlxei vsxei){
+
+  }
+  when(vlr vsr )
 }
